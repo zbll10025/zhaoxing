906dcc4d402f98d43c8fc49344f3da6c1342ea54
zbll10025 <2957774212@qq.com>
2024-08-05 11:09:05 +0800

    111

--- zhaoxing/Assets/Script/Enemyai/Enemy/EnemyData/BaseEnemyData/BaseEnemy.cs
+++ zhaoxing/Assets/Script/Enemyai/Enemy/EnemyData/BaseEnemyData/BaseEnemy.cs
@@ -2,7 +2,7 @@ using Assets.Script.Enemy;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Unity.Mathematics;
 public class BaseEnemy : MonoBehaviour
 {
     public float speed;
@@ -30,6 +30,10 @@ public class BaseEnemy : MonoBehaviour
     [Header("子物体上组件相关")]
     public AttackAreac AttackAreac;
     public PlayerCheck playerCheck;
+    [Header("玩家相关")]
+    public GameObject player;
+    public Vector2 playerDirection;
+    public float playerDistance;
     protected  virtual void Awake()
     {
 
@@ -43,6 +47,7 @@ public class BaseEnemy : MonoBehaviour
         currentsSpeed = patorlSpeed;
         //之后补地面与墙的全检测
         isGround = true;
+        ishit = false;
 
 
     }
@@ -51,14 +56,111 @@ public class BaseEnemy : MonoBehaviour
     protected virtual void Update()
     {
 
+        //死亡判定按键L
         if(Input.GetKeyDown(KeyCode.L)){
             isdead = true;
         }
+        //怪物受伤判定
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+           
+            Onhit();
+        }
 
     }
+     public virtual void Onhit()
+        {
+            ishit = true;
+            Vector2 a = -playerDirection * hitForce;
+            rig.AddForce(a, ForceMode2D.Impulse);
+            hp -= 20;
+        //受伤转身
+            if (!isRightLocalscal)
+            {
+                if (-playerDirection.x > 0)
+                {
+                    this.gameObject.transform.localScale = new Vector3(1, 1);
+                }
+                else
+                {
+                    this.gameObject.transform.localScale = new Vector3(-1, 1);
+                }
+            }
+            else
+            {
+                if (playerDirection.x > 0)
+                {
+                    this.gameObject.transform.localScale = new Vector3(1, 1);
+                }
+                else
+                {
+                    this.gameObject.transform.localScale = new Vector3(-1, 1);
+                }
 
+            }
+
+
+        }
     public virtual void Cancel_isHit()
     {
         ishit = false;
     }
+
+  
+         public void FixDirc()
+            {
+                LandMost landEnemy = this.GetComponent<LandMost>();
+
+                if (!landEnemy.isRightLocalscal)
+                {
+                    if (-landEnemy.playerDirection.x > 0)
+                    {
+                        landEnemy.gameObject.transform.localScale = new Vector3(1, 1, 1);
+                    }
+                    else
+                    {
+                        landEnemy.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+                    }
+                }
+                else
+                {
+                    if (landEnemy.playerDirection.x > 0)
+                    {
+                        landEnemy.gameObject.transform.localScale = new Vector3(1, 1, 1);
+                    }
+                    else
+                    {
+                        landEnemy.gameObject.transform.localScale = new Vector3(-1, 1, 1);
+                    }
+                }
+            }
+
+   
+
+    public void GetPlayerDistance()
+    {
+        GetPlayerObject();
+        if (player != null)
+            playerDistance = math.abs(player.transform.position.x - Enemytransform.position.x);
+    }
+
+    public void GetPlayerDirction()
+    {
+        GetPlayerObject();
+        if (player != null)
+            playerDirection = (player.transform.position - Enemytransform.position).normalized;
+    }
+
+    public void GetPlayerObject()
+    {
+        if (player == null)
+        {
+            player = GameObject.Find("Player");
+
+        }
+    }
+
+   
+
+
 }
--- zhaoxing/Assets/Script/Enemyai/Enemy/EnemyData/BaseEnemyData/LandMost.cs
+++ zhaoxing/Assets/Script/Enemyai/Enemy/EnemyData/BaseEnemyData/LandMost.cs
@@ -14,16 +14,15 @@ public class LandMost : BaseEnemy
        //前十个地面怪物检测组件的方法在各自的子类里
         public BehaviorTree behaviorTree;
 
-        public GameObject player;
-        public Vector2 playerDirection;
-        public float playerDistance;
+        
+        
        
        
         protected override void Awake()
         {
            base.Awake();
            player = GameObject.Find("Player");
-           ishit=false;
+          
 
         }  
 
@@ -34,95 +33,17 @@ public class LandMost : BaseEnemy
             GetPlayerObject();
             GetPlayerDirction();
             GetPlayerDistance();
-        //按空格受伤触发
-        if (Input.GetKeyDown(KeyCode.P)){
-
-            Onhit();
-        }
-
             
         }
 
-        public virtual void Onhit()
-        {
-            ishit = true;
-            Vector2 a = -playerDirection*hitForce;
-            rig.AddForce(a, ForceMode2D.Impulse);
-            hp -= 20;
-        if (!isRightLocalscal)
-        {
-            if (-playerDirection.x > 0)
-            {
-                this.gameObject.transform.localScale = new Vector3(1, 1);
-            }
-            else
-            {
-                this.gameObject.transform.localScale = new Vector3(-1, 1);
-            }
-        }
-        else {
-            if (playerDirection.x > 0)
-            {
-                this.gameObject.transform.localScale = new Vector3(1, 1);
-            }
-            else
-            {
-                this.gameObject.transform.localScale = new Vector3(-1, 1);
-            }
+     
 
-        }
+  
+       
 
+       
 
-    }
-
-    public void GetPlayerObject(){
-            if (player == null)
-            {
-                player = GameObject.Find("Player");
-
-            }
-        }
-        public void GetPlayerDirction()
-        {
-        GetPlayerObject();
-        if (player != null)
-           playerDirection = (player.transform.position - Enemytransform.position).normalized;
-        }
-
-        public void GetPlayerDistance()
-        {
-        GetPlayerObject();
-        if (player != null)
-             playerDistance = math.abs(player.transform.position.x - Enemytransform.position.x);
-        }
-
-        public void FixDirc()
-        {
-          LandMost landEnemy = this.GetComponent<LandMost>();
         
-        if (!landEnemy.isRightLocalscal)
-        {
-            if (-landEnemy.playerDirection.x > 0)
-            {
-                landEnemy.gameObject.transform.localScale = new Vector3(1, 1, 1);
-            }
-            else
-            {
-                landEnemy.gameObject.transform.localScale = new Vector3(-1, 1, 1);
-            }
-        }
-        else
-        {
-            if (landEnemy.playerDirection.x > 0)
-            {
-                landEnemy.gameObject.transform.localScale = new Vector3(1, 1, 1);
-            }
-            else
-            {
-                landEnemy.gameObject.transform.localScale = new Vector3(-1, 1, 1);
-            }
-        }
-    } 
 
        
 
